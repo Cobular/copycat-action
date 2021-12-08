555eaff111f0cc0802f4ccf1adab21bd517744d6
Cobular <Cobular@users.noreply.github.com>
2021-12-08 14:39:16 -0800

    Update file(s) "./Types" from ""

---
+++ media-2/plugins/Fuses.d.ts
@@ -0,0 +1,58 @@
+declare type Timezone = string
+
+//UserAdd
+
+declare interface UserAddProps {
+    presets?: {
+        userID?: string,
+        timezone?: Timezone
+    },
+    closeOnAdd?: boolean
+}
+
+declare type UserAddErrorHandler = string
+
+declare function UserAdd(props: UserAddProps): ReactComponent
+
+
+//Timer
+
+declare interface TimerProps {
+    timezone?: Timezone,
+    showSeconds?: boolean,
+    className?: string,
+    [key: string]: any
+}
+
+declare function Timer(props: TimerProps): ReactComponent
+
+
+//BasicTimer
+
+interface BasicTimerProps extends TimerProps {
+    tooltip?: boolean,
+    className?: string
+    staticTime?: Date
+}
+
+declare function BasicTimer(props: BasicTimerProps): ReactComponent
+
+
+//TimezoneManager
+declare function getOffset(date?: Date | moment.Moment):Timezone
+declare function getTimeFromTimezone(utcOffset: Timezone, currentDate?: Date): Date
+declare function getDateFromCity(city: string, sendAsMoment?: boolean): Date|moment.Moment
+declare function formatDate(date: string, timezone: Timezone): string
+
+
+//UserManager
+type UserList = {
+    [key: string]: Timezone
+}
+declare function addUser(id: string, utcOffset: Timezone)
+declare function removeUser(id: string)
+declare function getList(): UserList
+declare function getTimezone(id: string): Timezone
+
+//Alert
+declare function createQuestion(title: string | ReactElement, description: string | ReactElement, buttons: Array<{ text: string | ReactElement, color: string}>): Promise<string>
